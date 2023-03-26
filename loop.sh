592ac109b9a42b6e4c4d655c52066bf356fb95ea
Brandon <109305092@g.nccu.edu.tw>
2023-03-26 16:29:50 +0800

    fix: loop.sh

--- loop.sh
+++ loop.sh
@@ -3,9 +3,11 @@
 # 遍歷當前目錄下的所有子目錄，排除 .github 和 .git
 for d in */; do
     if [[ $d != ".github/" && $d != ".git/" ]]; then
+        cd $d
         docker compose up -d
         npm install
         npm run test
         docker compose down
+        cd ..
     fi
 done
